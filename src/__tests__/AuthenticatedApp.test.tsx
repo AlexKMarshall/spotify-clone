44cfec20f0fdcd134a2d2317aec1aecdd9a2ad37
Alex Marshall <alex.k.marshall83@gmail.com>
2020-09-19 19:01:50 +0100

    Switch on the test

--- src/__tests__/AuthenticatedApp.test.tsx
+++ src/__tests__/AuthenticatedApp.test.tsx
@@ -4,7 +4,7 @@ import * as db from '../test/data';
 import App from '../App';
 import { buildAlbum, buildTrack } from '../test/generate';
 
-xit('renders the app', async () => {
+it('renders the app', async () => {
   await db.newReleases.set([buildAlbum(), buildAlbum()]);
   await db.currentlyPlaying.set(buildTrack());
   await render(<App />);
