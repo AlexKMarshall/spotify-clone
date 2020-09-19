36fd46fb39e7f474e4fa2d61c7b0eb1b3b08115c
Alex Marshall <alex.k.marshall83@gmail.com>
2020-09-19 18:45:46 +0100

    Reinstate smoke test of authenticated app

--- src/__tests__/AuthenticatedApp.test.tsx
+++ src/__tests__/AuthenticatedApp.test.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { render, screen } from '../test/test-utils';
+import * as db from '../test/data';
 import App from '../App';
+import { buildAlbum, buildTrack } from '../test/generate';
 
 xit('renders the app', async () => {
+  await db.newReleases.set([buildAlbum(), buildAlbum()]);
+  await db.currentlyPlaying.set(buildTrack());
   await render(<App />);
 
   expect(screen.getByRole('link', { name: /your library/i })).toBeInTheDocument();
